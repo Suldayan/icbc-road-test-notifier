007b093b0bf9547736efcc5f14f26f08f41a3fd0
Timothy <timothysuldayan1@gmail.com>
2025-07-31 21:04:54 -0700

    configured initital project

---
+++ src/main/java/com/example/icbc_road_test_notifier/IcbcRoadTestNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.icbc_road_test_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcbcRoadTestNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcbcRoadTestNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/icbc_road_test_notifier/IcbcRoadTestNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icbc_road_test_notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcbcRoadTestNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
